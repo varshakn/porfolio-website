d989dd229bed1b88d541546868903ac27ed978e5
varshakn <knvarsha9@gmail.com>
2024-09-08 17:42:17 +0530

    Add favicon.ico to public directory

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the build output directory is 'dist'
+  },
+  // Uncomment the following if you are deploying to a subdirectory
+  // base: '/subdirectory/', 
 })
