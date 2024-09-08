d1c30dfb53ebe8759fe5ba9f85d02854ff8e7c15
varshakn <knvarsha9@gmail.com>
2024-09-08 15:55:17 +0530

    updated on 08-09-24

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,11 +1,10 @@
-// src/main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css'; // Include your styles if needed
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
